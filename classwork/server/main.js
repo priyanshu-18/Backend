fff582cc55dd5be87f8e19ac5b88fa66a089768b
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-02-16 11:56:36 +0530

    i just learn the res.on how to use how data goes in small amount of chunks

--- classwork/server/main.js
+++ classwork/server/main.js
@@ -1,8 +1,31 @@
 const http = require("http");
 const fs = require("fs");
 
+
 const myserver = http.createServer((req,res)=>{
-    res.end("server");
+    const user = {
+        name:"anything",
+        age:"anything"
+    };
+    const url = req.url;
+    const method = req.method;
+    if(url=="/users" && method=="GET"){
+        res.end(JSON.stringify(user));
+    }
+    if(url=="/data" && method=="POST"){
+        let body = "";
+        req.on("data",(chunk)=>{
+            body+=chunk;
+        })
+        req.on("end",()=>{
+            fs.writeFile("./file1.txt",body,()=>{
+                res.writeHead(201,"data saved succfull");
+                res.end();
+            });
+        });
+    }
+    
+    
 });
 
 myserver.listen(8000,()=>{ console.log("server started")});
