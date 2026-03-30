403557d87581c184f35b4e9d9b422ea7f88244fe
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-03-30 06:24:58 +0000

    leAMrne30 new l89gi158

---
+++ classwork/devconnect/Backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/",(req,res) =>{
+    res.send("hello from the server");
+});
+
