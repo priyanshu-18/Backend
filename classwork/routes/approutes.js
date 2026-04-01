b476cc2d5f28ab0aba9b76ed7c0a2c31a1ea91b7
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-04-01 06:16:28 +0000

    leAMrne1 new l91gi328

--- classwork/routes/approutes.js
+++ classwork/routes/approutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Example POST route
+
 router.post('/data', (req, res) => {
     res.json({
         message: "POST route working!",
