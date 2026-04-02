2bbd5149e8a747efe6de6362031545803d7f826c
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-04-02 07:07:29 +0000

    lePMrne2 new l92gi429

--- classwork/devconnect/Backend/app.js
+++ classwork/devconnect/Backend/app.js
@@ -8,3 +8,17 @@ app.get("/",(req,res) =>{
     res.send("hello from the server");
 });
 
+
+app.get("/me", (req, res) => {
+    const token = req.headers.authorization;
+    const user = users.find(user => user.token === token);
+    if (user) {
+        res.send({
+            username: user.username
+        })
+    } else {
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    }
+})
