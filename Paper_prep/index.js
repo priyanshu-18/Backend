f97c5163599a8abaa5ea198eccc89822db7f937d
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-05-15 20:25:59 +0000

    leAMrne16 new l136gi659

--- Paper_prep/index.js
+++ Paper_prep/index.js
@@ -2,34 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
 
+// Import JSON file
+const students = require("./students.json");
+
+
+// Serve static files
 app.use(express.static(__dirname));
-
-let students = [];
-
-
-// POST API
-app.post("/students", (req, res) => {
-
-    students.push(req.body);
-
-    res.json({
-        message: "Student Added"
-    });
-
-});
 
 
 // GET API
 app.get("/students", (req, res) => {
 
-    res.json(students);
+    res.send(students);
 
 });
 
 
-// SERVER
+// Server
 app.listen(3000, () => {
 
     console.log("Server Running");
